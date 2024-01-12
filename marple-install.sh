3824380b225e7628a2c85bc1936898a3407bb2c4
502107 <107959213+502107@users.noreply.github.com>
2024-01-12 10:31:09 +0000

    Install + run marple
    marple-install.sh installs dependencies, adds functions to .bashrc and creates mamba environment.
    marple.sh activates environment and runs marple.

---
+++ marple-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# last update: 12/01/2024
+
+pckg="snakemake bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython"
+
+if ! grep transfer-pgt ~/.bashrc &> /dev/null; then
+        echo -e '\nfunction transfer-pgt () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pgt/$3.fastq.gz\n}' >> ~/.bashrc
+        echo -e '\nfunction transfer-pst () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pst/$3.fastq.gz\n}' >> ~/.bashrc
+        echo -e '\nfunction marple() {\npushd /home/$USER/marple \n bash marple.sh $1 \n popd\n}' >> ~/.bashrc
+        source ~/.bashrc
+fi
+
+if command -v mamba &> /dev/null; then
+    if mamba env list | grep -q marple-env; then
+                exit 1
+        else
+                mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
+        fi
+elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+        echo "$OSTYPE"
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                sudo apt-get install bzip2
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+                brew install bzip2
+        fi
+        wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+        ./bin/micromamba shell init -s bash -p ~/micromamba
+        source ~/.bashrc
+	sleep 1
+       	micromamba create -n base -c conda-forge -y
+       	micromamba activate base
+       	micromamba install mamba -c conda-forge -y
+       	mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
+else
+        echo "Unsupported operating system"
+        exit 1
+fi
--- marple.sh
+++ marple.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# last update: 03/01/2024
+# last update: 12/01/2024
 
-cwd=$(pwd)
+source activate marple-env
 
-run_cmd="docker run -v $cwd:/workflow -w /workflow marple snakemake --cores all"
+run_cmd="snakemake --cores all"
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --dev)
-            run_cmd="docker run -v $cwd:/workflow -w /workflow -it marple /bin/bash"
+            run_cmd="/bin/bash"
             shift
             ;;
         *)
@@ -18,50 +18,15 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-if command -v docker &> /dev/null; then
-    echo "Skipping Docker installation"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Detected Linux"
-    sudo apt update
-    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Detected macOS"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install --cask docker
-else
-    echo "Unsupported operating system"
-    exit 1
-fi
-
-# Create a docker img of snakemake if it doesn't exist
-
-if docker images | grep marple > /dev/null; then
-	$run_cmd
-else
-	git clone https://github.com/snakemake/snakemake.git
-	cd snakemake/
-	if [[  "$OSTYPE" == "linux-gnu"* ]]; then
-		sed -i 's/apptainer/apptainer -c bioconda bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython/g' Dockerfile
-		sudo groupadd -f docker
-		sudo usermod -aG docker $USER
-    		sg docker -c '
-       		docker build -t marple ./
-        	cd ..
-        	rm -rf snakemake
-        	$run_cmd'
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		sed -i -tmp 's/apptainer/apptainer -c bioconda bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython/g' Dockerfile
-		rm Dockerfile-tmp
-		docker build -t marple ./
-		cd ..
-		rm -rf snakemake
+if command -v mamba &> /dev/null; then
+    if mamba env list | grep -q marple-env; then
 		$run_cmd
 	else
-		echo "Unsupported operating system"
+		echo "Environment marple-env not found."
+        exit 0
 	fi
+else
+    echo "Mamba not found."
+    exit 0
 fi
 
