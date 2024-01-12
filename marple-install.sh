b02daf9046bef26fcedbee31037165caf751d8ba
502107 <107959213+502107@users.noreply.github.com>
2024-01-12 15:06:12 +0000

    Update marple-install.sh

--- marple-install.sh
+++ marple-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # last update: 12/01/2024
 
-wd=home/$USER/marple
+wd=/home/$USER/marple
+cwd=$(pwd)
 
-if [ $(pwd) != $wd ]; then
-        echo "The marple directory should be in /home/$USER/marple. Move and try again."
-        exit 1
+if [ $cwd != $wd ]; then
+        echo "The marple directory should be in /home/$USER/marple. Move and try again"
+        :
 fi
 
 pckg="snakemake bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython"
@@ -13,13 +14,13 @@ pckg="snakemake bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl
 if ! grep transfer-pgt ~/.bashrc &> /dev/null; then
         echo -e '\nfunction transfer-pgt () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pgt/$3.fastq.gz\n}' >> ~/.bashrc
         echo -e '\nfunction transfer-pst () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pst/$3.fastq.gz\n}' >> ~/.bashrc
-        echo -e '\nfunction marple() {\npushd /home/$USER/marple \n bash marple.sh $1 \n popd\n}' >> ~/.bashrc
+        echo -e '\nfunction marple() {\npushd /home/$USER/marple \nmamba activate marple-env \nbash marple.sh $1 \nmamba deactivate \npopd\n}' >> ~/.bashrc
         source ~/.bashrc
 fi
 
 if command -v mamba &> /dev/null; then
-    if mamba env list | grep -q marple-env; then
-                exit 1
+if mamba env list | grep -q marple-env; then
+                :
         else
                 mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
         fi
@@ -34,11 +35,12 @@ elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
         wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
         ./bin/micromamba shell init -s bash -p ~/micromamba
         source ~/.bashrc
-	sleep 1
-       	micromamba create -n base -c conda-forge -y
-       	micromamba activate base
-       	micromamba install mamba -c conda-forge -y
-       	mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
+        sleep 1
+        micromamba create -n base -c conda-forge -y
+        micromamba activate base
+        micromamba install mamba -c conda-forge -y
+        mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
+        mamba init
 else
         echo "Unsupported operating system"
         exit 1
