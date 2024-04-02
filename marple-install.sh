2c91c0a097170fdc5b4138994989862f83c2ce4b
502107 <107959213+502107@users.noreply.github.com>
2024-04-02 10:44:44 +0100

    Add files via upload

--- marple-install.sh
+++ marple-install.sh
@@ -23,6 +23,7 @@ fi
 marple_func='\nfunction marple() {\npushd ~/marple \nmamba activate marple-env \ncat .version.info \nbash marple.sh $1 \nmamba deactivate \npopd\n}'
 transfer_pst_func='\nfunction transfer-pgt() {\nexperiment=$1\nshift\nfor arg in "$@";do\nIFS="=" read -r barcode sample<<<"$arg"\nfind /var/lib/minknow/data/* -type d -name "$experiment" 2>/dev/null | xargs -I {} find '{}' -type d -name basecalling | while read dir; do\ncat $dir/pass/"$barcode"/*.fastq.gz > ~/marple/reads/pgt/"$sample".fastq.gz\ndone\ndone\n}'
 transfer_pgt_func='\nfunction transfer-pst() {\nexperiment=$1\nshift\nfor arg in "$@";do\nIFS="=" read -r barcode sample<<<"$arg"\nfind /var/lib/minknow/data/* -type d -name "$experiment" 2>/dev/null | xargs -I {} find '{}' -type d -name basecalling | while read dir; do\ncat $dir/pass/"$barcode"/*.fastq.gz > ~/marple/reads/pst/"$sample".fastq.gz\ndone\ndone\n}'
+auspice_func='\nfunction view-marple-tree() {\nauspice view --datasetDir ~/marple/results/auspice/ & xdg-open http://localhost:4000\n}
 
 # Create backup of .bashrc and add marple functions
 if [[ ! -d ".marple-tmp" ]] && [[ $(grep -L transfer-pgt "$bashf") ]]; then
@@ -31,9 +32,11 @@ if [[ ! -d ".marple-tmp" ]] && [[ $(grep -L transfer-pgt "$bashf") ]]; then
         echo -e "$marple_func" >> "$bashf"
         echo -e "$transfer_pst_func" >> "$bashf"
         echo -e "$transfer_pgt_func" >> "$bashf"
+        echo -e "$auspice_func" >> "$bashf"
         echo -e '\nexport -f marple' >> "$bashf"
         echo -e 'export -f transfer-pst' >> "$bashf"
-        echo -e 'export -f transfer-pgt\n' >> "$bashf"
+        echo -e 'export -f transfer-pgt' >> "$bashf"
+        echo -e 'export -f view-marple-tree\n' >> "$bashf"
         source "$bashf"
 elif [[ $(grep transfer-pgt "$bashf") ]]; then
         echo "Do you want to update the functions for MARPLE?"
@@ -41,21 +44,23 @@ elif [[ $(grep transfer-pgt "$bashf") ]]; then
         case $yn in
         Yes )
         mkdir -p .marple-tmp
-        sed -i-e '/function marple() {/,/export -f transfer-pgt/d' "$bashf";
+        sed -i-e '/function marple() {/,/export -f view-marple-tree/d' "$bashf";
         mv "$bashf"-e .marple-tmp/bashrc.bak
         echo -e "$marple_func" >> "$bashf";
         echo -e "$transfer_pst_func" >> "$bashf";
         echo -e "$transfer_pgt_func" >> "$bashf";
+        echo -e "$auspice_func" >> "$bashf"
         echo -e '\nexport -f marple' >> "$bashf"
         echo -e 'export -f transfer-pst' >> "$bashf"
-        echo -e 'export -f transfer-pgt\n' >> "$bashf"
+        echo -e 'export -f transfer-pgt' >> "$bashf"
+        echo -e 'export -f view-marple-tree\n' >> "$bashf"
         break;;
         No ) break;;
         esac
         done
 fi
 
-pckg="snakemake bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython"
+pckg="python=3.10 augur snakemake bwa star samtools nanoq fastqc gffread multiqc fasttree openpyxl matplotlib biopython"
 
 if command -v mamba &> /dev/null; then
         if mamba env list | grep -q marple-env; then
@@ -65,7 +70,13 @@ if command -v mamba &> /dev/null; then
         fi
 elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/linux | bash
+                printf '\n%s\n' 'eval "$("/home/marple/.nextstrain/cli-standalone/nextstrain" init-shell bash)"' >> ~/.bashrc
+                eval "$("/home/marple/.nextstrain/cli-standalone/nextstrain" init-shell bash)"
                 sudo apt-get install bzip2
+                sudo apt-get install gnumeric
+                sudo apt install npm
+                sudo npm install --global auspice
                 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
                 ./bin/micromamba shell init -s bash -p ~/micromamba
                 source "$bashf"
@@ -76,6 +87,8 @@ elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
                 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
                 brew install bzip2
                 brew install micromamba
+                brew install gnumeric
+                curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/mac | bash
                 eval "$(micromamba shell hook --shell bash)"
                 micromamba activate
         fi
