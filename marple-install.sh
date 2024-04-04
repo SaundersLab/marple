2f7c39bd6e06e1abadb43cf86949106838411faa
502107 <107959213+502107@users.noreply.github.com>
2024-04-04 11:55:32 +0100

    More options for updating dependencies

--- marple-install.sh
+++ marple-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# last update: 21/03/2024
+# last update: 02/04/2024
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         wd=/home/$USER/marple
@@ -16,14 +16,10 @@ if [ $cwd != $wd ]; then
         exit 1
 fi
 
-# These old transfer-p[g/s]t may be useful for future GUI implementation
-# echo -e '\nfunction transfer-pgt () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pgt/$3.fastq.gz\n}' >> ~/.bashrc
-# echo -e '\nfunction transfer-pst () {\ncat /var/lib/minknow/data/reads/$1/*/*/basecalling/pass/$2/*.fastq.gz > /home/$USER/marple/reads/pst/$3.fastq.gz\n}' >> ~/.bashrc
-
 marple_func='\nfunction marple() {\npushd ~/marple \nmamba activate marple-env \ncat .version.info \nbash marple.sh $1 \nmamba deactivate \npopd\n}'
 transfer_pst_func='\nfunction transfer-pgt() {\nexperiment=$1\nshift\nfor arg in "$@";do\nIFS="=" read -r barcode sample<<<"$arg"\nfind /var/lib/minknow/data/* -type d -name "$experiment" 2>/dev/null | xargs -I {} find '{}' -type d -name basecalling | while read dir; do\ncat $dir/pass/"$barcode"/*.fastq.gz > ~/marple/reads/pgt/"$sample".fastq.gz\ndone\ndone\n}'
 transfer_pgt_func='\nfunction transfer-pst() {\nexperiment=$1\nshift\nfor arg in "$@";do\nIFS="=" read -r barcode sample<<<"$arg"\nfind /var/lib/minknow/data/* -type d -name "$experiment" 2>/dev/null | xargs -I {} find '{}' -type d -name basecalling | while read dir; do\ncat $dir/pass/"$barcode"/*.fastq.gz > ~/marple/reads/pst/"$sample".fastq.gz\ndone\ndone\n}'
-auspice_func='\nfunction view-marple-tree() {\nauspice view --datasetDir ~/marple/results/auspice/ & xdg-open http://localhost:4000\n}
+auspice_func='\nfunction view-marple-tree() {\nauspice view --datasetDir ~/marple/results/auspice/ & xdg-open http://localhost:4000\n}'
 
 # Create backup of .bashrc and add marple functions
 if [[ ! -d ".marple-tmp" ]] && [[ $(grep -L transfer-pgt "$bashf") ]]; then
@@ -39,10 +35,10 @@ if [[ ! -d ".marple-tmp" ]] && [[ $(grep -L transfer-pgt "$bashf") ]]; then
         echo -e 'export -f view-marple-tree\n' >> "$bashf"
         source "$bashf"
 elif [[ $(grep transfer-pgt "$bashf") ]]; then
-        echo "Do you want to update the functions for MARPLE?"
-        select yn in "Yes" "No"; do
+        while true; do
+        read -p "Do you want to update the functions for MARPLE? " yn
         case $yn in
-        Yes )
+        [Yy]* )
         mkdir -p .marple-tmp
         sed -i-e '/function marple() {/,/export -f view-marple-tree/d' "$bashf";
         mv "$bashf"-e .marple-tmp/bashrc.bak
@@ -55,7 +51,8 @@ elif [[ $(grep transfer-pgt "$bashf") ]]; then
         echo -e 'export -f transfer-pgt' >> "$bashf"
         echo -e 'export -f view-marple-tree\n' >> "$bashf"
         break;;
-        No ) break;;
+        [Nn]* ) break;;
+        * ) echo "Please select an option [Yn] ";;
         esac
         done
 fi
@@ -64,15 +61,43 @@ pckg="python=3.10 augur snakemake bwa star samtools nanoq fastqc gffread multiqc
 
 if command -v mamba &> /dev/null; then
         if mamba env list | grep -q marple-env; then
-                :
+                while true; do
+                read -p "Do you want to update the marple-env? " yn
+                case $yn in
+                [Yy]* )
+                mamba env remove -n marple-env -y -q
+                mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
+                break;;
+                [Nn]* ) while true; do
+                read -p "Do you want to update the softwares used in MARPLE? " yn
+                case $yn in
+                [Yy]* ) 
+                if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+                sudo apt-get install bzip2
+                sudo apt-get install gnumeric
+                sudo apt install npm
+                sudo npm install --global auspice
+                elif [[ "$OSTYPE" == "darwin"* ]]; then
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+                brew install bzip2
+                brew install gnumeric
+                brew install npm
+                npm install --global auspice
+                fi 
+                break;;
+                [Nn]* ) break;;
+                * ) echo "Please select an option [Yn] ";;
+                esac
+                done
+                break;;
+                * ) echo "Please select an option [Yn] ";;
+                esac
+                done
         else
                 mamba create -n marple-env -y -c bioconda -c conda-forge $pckg
         fi
 elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
-        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-                curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/linux | bash
-                printf '\n%s\n' 'eval "$("/home/marple/.nextstrain/cli-standalone/nextstrain" init-shell bash)"' >> ~/.bashrc
-                eval "$("/home/marple/.nextstrain/cli-standalone/nextstrain" init-shell bash)"
+        if [[ "$OSTYPE" == "linux-gnu"* ]] && [[ ! -d ~/micromamba ]]; then
                 sudo apt-get install bzip2
                 sudo apt-get install gnumeric
                 sudo apt install npm
@@ -83,11 +108,13 @@ elif [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
                 sleep 1
                 micromamba create -n base -c conda-forge -y
                 micromamba activate base
-        elif [[ "$OSTYPE" == "darwin"* ]]; then
+        elif [[ "$OSTYPE" == "darwin"* ]] && ! command -v micromamba &> /dev/null; then
                 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
                 brew install bzip2
                 brew install micromamba
                 brew install gnumeric
+                brew install npm
+                npm install --global auspice
                 curl -fsSL --proto '=https' https://nextstrain.org/cli/installer/mac | bash
                 eval "$(micromamba shell hook --shell bash)"
                 micromamba activate
