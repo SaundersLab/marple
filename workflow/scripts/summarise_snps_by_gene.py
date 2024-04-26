641827517321fd651dd934596028a10763b6bc16
502107 <107959213+502107@users.noreply.github.com>
2024-04-26 16:57:49 +0100

    Bug fix

--- workflow/scripts/summarise_snps_by_gene.py
+++ workflow/scripts/summarise_snps_by_gene.py
@@ -12,7 +12,7 @@ for snps_path in snps_paths:
     snps_path = str(snps_path)
     df = pd.read_csv(snps_path, sep='\t')
     snps = df[df.ref != df.alleles]
-    sample = snps_path.split('/')[-1].split('.')[0]
+    sample = snps_path.split('/')[-1].rsplit('.')[0]
     sample_n_snps = pd.DataFrame({sample: snps.groupby('seqid').size()})
     n_snps = n_snps.join(sample_n_snps).fillna(0)
 for col in n_snps:
