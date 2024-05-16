cae50f3ec57156017c45ac60d2a900c6fcad37fa
502107 <107959213+502107@users.noreply.github.com>
2024-05-16 16:40:36 +0100

    Testing description of fungicide-resistance genes

--- workflow/scripts/create_sample_multiqc_row.py
+++ workflow/scripts/create_sample_multiqc_row.py
@@ -1,9 +1,24 @@
 import pandas as pd
-from Bio.SeqIO import parse
+from Bio import SeqIO
 import os
+import multiqc
+from multiqc.plots import table
+from multiqc.modules.base_module import BaseMultiqcModule
 
+def position_range(position):
+    if '-' in str(position):
+        start, end = map(int, position.split('-'))
+        return list(range(start, end + 1))
+    else:
+        return [int(position)]
+    
 tree_input_path = str(snakemake.input.tree_input)
-tree_input_seq = next(str(r.seq) for r in parse(tree_input_path, 'fasta'))
+metadata_path = str(snakemake.input.metadata)
+consensus_path = str(snakemake.input.consensus)
+fung_output_path = str(snakemake.output.multiqc_fung)
+
+metadata_df = pd.read_excel(metadata_path)
+tree_input_seq = next(str(r.seq) for r in SeqIO.parse(tree_input_path, 'fasta'))
 pct_tree_bases = 100 * (1 - tree_input_seq.count('N') / len(tree_input_seq))
 
 groupby_summaries = [pd.DataFrame(
@@ -26,3 +41,46 @@ for i, path in enumerate(snakemake.input.primer_performance_summaries):
         for val in primers[groupby_col].values
     }))
 pd.concat(groupby_summaries, axis=1).to_csv(snakemake.output.multiqc_row, index=None)
+
+# Adding fungicide resistance genes records to the multiqc report
+
+sequences = {record.id: str(record.seq) for record in SeqIO.parse(consensus_path, 'fasta')}
+
+matching_rows = []
+known_mutations = set(metadata_df['Nucleotide Position'])
+
+for idx, row in metadata_df.iterrows():
+    gene = row['Gene']
+    nuc_pos = position_range(row['Nucleotide Position'])
+    old_nuc = row['Old Nucleotide']
+    new_nuc = row['New Nucleotide']
+    
+    for sample_name, sequence in sequences.items():
+        if gene == sample_name:
+            for pos in nuc_pos:
+                print(f'Checking {sample_name} at position {pos} in {gene} (with length {len(sequence)})')
+                if sequence[pos - 1] == new_nuc:
+                    matching_rows.append({
+                        'Sample Name': snakemake.wildcards.sample,
+                        **row.to_dict()
+                    })
+
+# Check for SNPs not in the metadata
+for sample_name, sequence in sequences.items():
+    if gene == sample_name:
+        for i, nuc in enumerate(sequence):
+            if nuc != tree_input_seq[i] and i not in known_mutations:
+                matching_rows.append({
+                    'Sample Name': snakemake.wildcards.sample,
+                    'Gene': sample_name,
+                    'Nucleotide Position': i,
+                    'Old Nucleotide': tree_input_seq[i],
+                    'New Nucleotide': nuc,
+                    'Amino Acid Position': '?',
+                    'Old AminoAcid': '...',
+                    'New AminoAcid': '...'
+                })
+
+matching_df = pd.DataFrame(matching_rows)
+matching_df.to_csv(fung_output_path, index=None)
+
