b9f213f977ccaafb6f0b418c8e81039baafb686c
502107 <107959213+502107@users.noreply.github.com>
2024-09-11 20:21:51 +0500

    Update marple.sh
    Bug fixes

--- marple.sh
+++ marple.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # last update: 09/09/2024
 
-run_cmd="nohup snakemake --cores all --rerun-incomplete > snakemake.stdout 2> snakemake.stderr &"
+run_cmd="nohup snakemake --cores all --rerun-incomplete"
 
 if command -v mamba &> /dev/null; then
     if mamba env list | grep -q marple-env; then
     	conda activate marple-env > /dev/null 2>&1
-		$run_cmd 2> /dev/null &
+	$run_cmd 2> /dev/null &
         pid=$!
         spin='-\|/'
         i=0
@@ -15,8 +15,7 @@ if command -v mamba &> /dev/null; then
             i=$(( (i+1) %4 ))
             printf "\r${spin:$i:1}"
             sleep .1
-        done 
-        mv nohup.out logs/snakemake.out
+        done
         exit 0
 	else
 		echo "Environment marple-env not found."
